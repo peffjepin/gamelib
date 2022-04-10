5696ced6b10fe5eca0a8c7298898ec420c71f9af
peffjepin <peffjepin@gmail.com>
2022-04-09 23:25:15 -0400

    update setup.py extension configuration

--- setup.py
+++ setup.py
@@ -1,36 +1,14 @@
 import sys
 import setuptools
-import subprocess
-import shlex
 
 is_posix = sys.platform.lower() == "posix"
 
+flags = []
+links = []
+libraries = ["glfw"]
 
-class PkgConfig:
-    # namespace for pkg-config cli
-    @staticmethod
-    def _cmd(options, packages):
-        cmd = shlex.split(
-            f"pkg-config {' '.join(options)} {' '.join(packages)}"
-        )
-        p = subprocess.run(cmd, check=True, capture_output=True)
-        return p.stdout.decode("utf-8").rstrip()
-
-    @classmethod
-    def configure_extension(cls, extension, packages, options=()):
-        cflags_options = (*options, "--cflags")
-        libs_options = (*options, "--libs")
-
-        cflags = cls._cmd(cflags_options, packages).split()
-        libs = cls._cmd(libs_options, packages).split()
-
-        extension.extra_compile_args.extend(cflags)
-        extension.extra_link_args.extend(libs)
-
-
-flags = ["-std=c11", "-Wall"]
 if is_posix:
-    flags.append("-Wpedantic")
+    flags.extend(("-std=c11", "-Wall", "-Wpedantic"))
 
 window_module = setuptools.Extension(
     "window",
@@ -38,8 +16,9 @@ window_module = setuptools.Extension(
         "gamelib/extensions/window_module.c",
         "gamelib/extensions/glad.c"
     ],
+    libraries=libraries,
     extra_compile_args=flags,
-    extra_link_args=["-lglfw"]
+    extra_link_args=links,
 )
 
 
